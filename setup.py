882d856a58e7ef3c679421bf9e605e9ba6be236f
Patrick Altman <paltman@gmail.com>
2015-12-09 15:15:15 -0600

    0.4.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     description="",
     name="pinax-pages",
     long_description=read("README.rst"),
-    version="0.4.0",
+    version="0.4.1",
     url="http://pinax-pages.rtfd.org/",
     license="MIT",
     packages=find_packages(),
